b60eae95a8ad5defcfe565fd1765be40cae0fb34
Lucas <lucasalves_taz@hotmail.com>
2025-09-10 20:35:47 -0300

    add app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 # DataApp
+################################
+# Bibliotecas
+################################
+import streamlit as st
+from list_stock import list_stock
+
+################################
+# Configuração da página
+################################
+st.set_page_config(page_title="DataApp", page_icon="📊", layout="wide")
+
+################################
+# Título do App
+################################
+st.title("DataApp Stock Analysis 📊")
+
+################################
+# Construção do App - Sidebar
+################################
+
+stock = st.sidebar.selectbox("Ticker", list_stock)
